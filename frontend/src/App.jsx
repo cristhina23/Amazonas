7fd52151a268c2324ae824a044f269a94fd4b3fd
cristhina23 <viszellechacon@gmail.com>
2023-09-19 22:00:02 -0500

    act

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Link } from "react-router-dom";
+import data from "./data";
 
 function App() {
   return (
@@ -7,6 +8,31 @@ function App() {
       <header>
         <a href='#'>Amazona</a>
       </header>
+      <main>
+        <h1>Welcome to Amazona</h1>
+        <div className='products'>
+          {data.products.map((product) => (
+            <div
+              className='product'
+              key={product.slug}
+            >
+              <a href={`/product/${product.slug}`}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                />
+              </a>
+              <div className='product-info'>
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>${product.price}</p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </>
   );
 }
