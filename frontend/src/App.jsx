6243708865259cd0f347a298d7581b5b29eb16e6
cristhina23 <viszellechacon@gmail.com>
2023-09-19 21:20:50 -0500

    inicio

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <header>
+        <a href='#'>Amazona</a>
+      </header>
+    </>
+  );
+}
+
+export default App;
